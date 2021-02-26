ea46e858c5ef5f94a90ea55607752bb40c257c47
leandrocodes <leandrovianacodes@gmail.com>
2021-02-26 14:25:28 -0400

    field component

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 
 import params from '../params'
 
-export default () => {
+export default ({ mined, opened, nearMines }) => {
   const styleField = [styles.field]
 
+  if (opened) styleField.push(styles.openField)
   if (styleField.length === 1) styleField.push(styles.fieldRegular)
 
-  return <View style={styleField}></View>
+  let color = null
+  if (nearMines > 0) {
+    if (nearMines === 1) color = '#2A28D7'
+    if (nearMines === 2) color = '#2b520f'
+    if (nearMines > 2 && nearMines < 6) color = '#f9060a'
+    if (nearMines >= 6) color = '#f221a9'
+  }
+
+  return (
+    <View style={styleField}>
+      {!mined && opened && nearMines > 0 ? (
+        <Text style={[styles.label, { color }]}>{nearMines}</Text>
+      ) : (
+        false
+      )}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -23,5 +40,17 @@ const styles = StyleSheet.create({
     borderTopColor: '#CCC',
     borderRightColor: '#333',
     borderBottomColor: '#333'
+  },
+
+  openField: {
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize
   }
 })
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,6 +11,13 @@ export default () => (
       Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
     </Text>
     <Field />
+    <Field opened />
+    <Field opened nearMines={1} />
+    <Field opened nearMines={2} />
+    <Field opened nearMines={3} />
+    <Field opened nearMines={4} />
+    <Field opened nearMines={5} />
+    <Field opened nearMines={6} />
   </View>
 )
 
