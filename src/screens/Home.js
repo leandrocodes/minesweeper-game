5073124ef588dade668cf21c11fc07bd84c47da3
leandrocodes <leandrovianacodes@gmail.com>
2021-02-26 14:01:02 -0400

    grid params

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { View, Text, StyleSheet, Platform, StatusBar } from 'react-native'
+import params from '../params'
 
 export default () => (
   <View style={styles.view}>
-    <Text>Eaí</Text>
+    <Text style={styles.welcome}>Starting mines!</Text>
+    <Text style={styles.instructions}>
+      Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+    </Text>
   </View>
 )
 
 const styles = StyleSheet.create({
   view: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
